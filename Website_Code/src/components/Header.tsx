51a3921325d925887c0f4f4354e9d6e296e8de70
anirudhreddyjakka <anirudhreddyjakka001@gmail.com>
2025-03-16 16:10:52 +0530

    added info page and sandeep:routers

--- Website_Code/src/components/Header.tsx
+++ Website_Code/src/components/Header.tsx
@@ -11,9 +11,10 @@ const Header = () => {
             <span className="text-2xl font-bold text-blue-900">IIT Tirupati Water Monitor</span>
           </div>
           <div className="hidden md:flex space-x-8">
-            <a href="#home" className="text-gray-600 hover:text-blue-600">Home</a>
-            <a href="#statistics" className="text-gray-600 hover:text-blue-600">Statistics</a>
-            <a href="#about" className="text-gray-600 hover:text-blue-600">About</a>
+            <a href="/" className="text-gray-600 hover:text-blue-600">Home</a>
+            <a href="/statistics" className="text-gray-600 hover:text-blue-600">Statistics</a>
+            <a href="/about" className="text-gray-600 hover:text-blue-600">About</a>
+            <a href="/tests" className="text-gray-600 hover:text-blue-600">Tests</a>
           </div>
         </div>
       </nav>
---
+++ Website_Code/src/components/homepage.tsx
@@ -0,0 +1,16 @@
+
+import Header from './Header';
+import Hero from './Hero';
+import Statistics from './Statistics';
+import About from './About';
+
+const Homepage = () => {
+    return(
+    <div>
+        <Hero/>
+        <Statistics/>
+        <About/>
+    </div>
+    );
+};
+export default Homepage;
