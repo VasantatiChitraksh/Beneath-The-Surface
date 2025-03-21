7348a9bc71b15cb9146e63e26d33d994008a692a
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2025-03-21 15:55:55 +0530

    Tests.tsx table values are correctly updated

--- Website_Code/tailwind.config.js
+++ Website_Code/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+       },
+    }
   },
   plugins: [],
 };
+
