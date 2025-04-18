0ce77add9a52a1b473b6f3e94384a584e3c7c468
nithishgouds <srinithishgoudsuragouni@gmail.com>
2025-04-18 18:52:20 +0530

    update

--- Python_Code/server.py
+++ Python_Code/server.py
@@ -3,14 +3,7 @@ import os
 
 app = Flask(__name__, static_folder="static")
 
-# Route to serve static files (images) from the 'static/images/' directory
 @app.route('/static/images/<path:filename>')
 def serve_static(filename):
-    # Ensure that the static directory exists
     image_dir = os.path.join(app.static_folder, 'images')
     return send_from_directory(image_dir, filename)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
