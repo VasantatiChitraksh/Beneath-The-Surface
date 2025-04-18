8777b8a072e70cd50829345c343982a0dab5cce7
nithishgouds <srinithishgoudsuragouni@gmail.com>
2025-04-18 19:16:19 +0530

    server.py

--- Python_Code/server.py
+++ Python_Code/server.py
@@ -7,3 +7,7 @@ app = Flask(__name__, static_folder="static")
 def serve_static(filename):
     image_dir = os.path.join(app.static_folder, 'images')
     return send_from_directory(image_dir, filename)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)  # ✅ bind to 0.0.0.0
