471a7fe01c277771ab2fea8fc44827799364d927
nithishgouds <srinithishgoudsuragouni@gmail.com>
2025-04-18 22:50:07 +0530

    Update Statistics.tsx

--- Website_Code/src/components/Statistics.tsx
+++ Website_Code/src/components/Statistics.tsx
@@ -4,6 +4,7 @@ import { BarChart3Icon } from "lucide-react";
 // Define base URL for the Flask server
 const IMAGE_BASE_URL = "https://beneath-the-surface-backend.onrender.com/static/images/";
 
+
 const hostels = [
   "Kedhar",
   "Bhairav",
