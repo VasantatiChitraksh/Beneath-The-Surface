cecd1f4e2f6c29aa65aa3ea2215e269e954a59fb
nithishgouds <srinithishgoudsuragouni@gmail.com>
2025-04-18 15:52:34 +0530

    server.py

--- Python_Code/server.py
+++ Python_Code/server.py
@@ -11,4 +11,6 @@ def serve_static(filename):
     return send_from_directory(image_dir, filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Use the PORT environment variable provided by Render (or fallback to 5000 if not set)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
