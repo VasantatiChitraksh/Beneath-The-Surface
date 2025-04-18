021597e4760d96b8f3e263d79faff58d279a44b1
nithishgouds <srinithishgoudsuragouni@gmail.com>
2025-04-18 22:08:15 +0530

    cors

--- Python_Code/server.py
+++ Python_Code/server.py
@@ -1,7 +1,11 @@
 from flask import Flask, send_from_directory
+from flask_cors import CORS  # Import CORS to handle cross-origin requests
 import os
 
 app = Flask(__name__, static_folder="static")
+
+# Enable CORS for all routes
+CORS(app)
 
 @app.route('/static/images/<path:filename>')
 def serve_static(filename):
